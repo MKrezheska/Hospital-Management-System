b7f6cd8b4d88885e0a84fbdfd28b5edde580614a
Magdalena Krezheska <magdalna_krezeska@hotmail.com>
2019-08-27 20:05:27 +0200

    Style table Doctors/Index.cshtml

--- HMS/Models/Doctors.cs
+++ HMS/Models/Doctors.cs
@@ -10,25 +10,27 @@ namespace HMS.Models
     public class Doctors
     {
         public int Id { get; set; }
-
+        [Display(Name = "Име")]
         public string Name { get; set; }
-
+        [Display(Name = "Тел. бр")]
         public double PhoneNumber { get; set; }
 
         public bool Available { get; set; }
-
+        [Display(Name = "Слика")]
         public string Image { get; set; }
 
         public string Focus{ get; set; }
 
         [Display(Name= "Специјалност")]
         public int SpecialityId { get; set; }
+        [Display(Name = "Специјалност")]
 
         [ForeignKey("SpecialityId")]
         public virtual Specialties Specialties { get; set; }
 
         [Display(Name = "Субспецијалност")]
         public int SubSpecialtiesId { get; set; }
+        [Display(Name = "Субспецијалност")]
 
         [ForeignKey("SubSpecialtiesId")]
         public virtual SubSpecialties SubSpecialties { get; set; }
