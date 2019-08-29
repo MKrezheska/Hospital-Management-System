dfb82a48657314cde338f696a2629a9f0e3fbb81
Magdalena Krezheska <magdalna_krezeska@hotmail.com>
2019-08-29 18:07:33 +0200

    Style Doctors cards.

--- HMS/Models/ViewModel/DoctorsViewModel.cs
+++ HMS/Models/ViewModel/DoctorsViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,9 @@ namespace HMS.Models.ViewModel
     public class DoctorsViewModel
     {
         public Doctors Doctors { get; set; }
+        [Display(Name = "Специјалност")]
         public IEnumerable<Specialties> Specialties { get; set; }
+        [Display(Name = "Субспецијалност")]
         public IEnumerable<SubSpecialties> SubSpecialties { get; set; }
 
     }
