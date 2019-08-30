f93f84f1d6ce7d2483d871a1d4f47ba8eb579a73
dimitarmileski <dimit@r.com>
2019-08-30 13:45:17 +0200

    CSV Excel - not implemented properly Word - not implemented properly

--- HMS/Areas/Admin/Controllers/AppointmentsController.cs
+++ HMS/Areas/Admin/Controllers/AppointmentsController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using CsvHelper;
 using HMS.Data;
 using HMS.Models;
 using HMS.Models.ViewModel;
@@ -11,7 +13,14 @@ using HMS.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using NPOI.HSSF.Util;
+using NPOI.SS.UserModel;
+using NPOI.SS.Util;
+using NPOI.XSSF.UserModel;
+using NPOI.XWPF.UserModel;
 using Rotativa.AspNetCore;
+
 
 
 namespace HMS.Areas.Admin.Controllers
@@ -25,9 +34,17 @@ namespace HMS.Areas.Admin.Controllers
         private readonly ApplicationDbContext _db;
         private int PageSize = 3;
 
-        public AppointmentsController(ApplicationDbContext db)
+        IConfiguration Configuration;
+
+        private static string homePath = (Environment.OSVersion.Platform == PlatformID.Unix ||
+               Environment.OSVersion.Platform == PlatformID.MacOSX)
+             ? Environment.GetEnvironmentVariable("HOME")
+             : Environment.ExpandEnvironmentVariables("%HOMEDRIVE%%HOMEPATH%");
+
+        public AppointmentsController(ApplicationDbContext db, IConfiguration configuration)
         {
             _db = db;
+            Configuration = configuration;
         }
 
 
@@ -334,5 +351,198 @@ namespace HMS.Areas.Admin.Controllers
             return new ViewAsPdf(appointmentVM);
         }
 
+
+        public async Task<IActionResult> AppointmentCSV(int doctorPage = 1, string searchName = null, string searchEmail = null, string searchPhone = null, string searchDate = null)
+        {
+
+            System.Security.Claims.ClaimsPrincipal currentUser = this.User;
+            var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            AppointmentViewModel appointmentVM = new AppointmentViewModel()
+            {
+                Appointments = new List<Models.Appointments>()
+            };
+
+            appointmentVM.Appointments = _db.Appointments.Include(a => a.DoctorUser).ToList();
+            if (User.IsInRole(SD.AdminEndUser))
+            {
+                appointmentVM.Appointments = appointmentVM.Appointments.Where(a => a.DoctorId == claim.Value).ToList();
+            }
+
+            if (searchName != null)
+            {
+                appointmentVM.Appointments = appointmentVM.Appointments.Where(a => a.PatientName.ToLower().Contains(searchName.ToLower())).ToList();
+            }
+            if (searchEmail != null)
+            {
+                appointmentVM.Appointments = appointmentVM.Appointments.Where(a => a.PatientEmail.ToLower().Contains(searchEmail.ToLower())).ToList();
+            }
+            if (searchPhone != null)
+            {
+                appointmentVM.Appointments = appointmentVM.Appointments.Where(a => a.PatientPhoneNumber.ToLower().Contains(searchPhone.ToLower())).ToList();
+            }
+            if (searchDate != null)
+            {
+                try
+                {
+                    DateTime appDate = Convert.ToDateTime(searchDate);
+                    appointmentVM.Appointments = appointmentVM.Appointments.Where(a => a.AppointmentDate.ToShortDateString().Equals(appDate.ToShortDateString())).ToList();
+                }
+                catch (Exception ex)
+                {
+
+                }
+
+            }
+
+            var count = appointmentVM.Appointments.Count;
+
+            appointmentVM.Appointments = appointmentVM.Appointments.OrderBy(p => p.AppointmentDate)
+                .Skip((doctorPage - 1) * PageSize)
+                .Take(PageSize).ToList();
+
+            using (var writer = new StreamWriter(homePath + Configuration["DownloadsRelativePath"]))
+            using (var csv = new CsvWriter(writer))
+            {
+                csv.WriteRecords(appointmentVM.Appointments);
+            }
+
+            return RedirectToAction("Index", "Appointments");
+        }
+
+
+        public IActionResult AppointmentExcel()
+        {
+            var newFile = homePath + "\\Downloads\\" + "AppointmentExcel.xlsx";
+
+            using (var fs = new FileStream(newFile, FileMode.Create, FileAccess.Write))
+            {
+
+                IWorkbook workbook = new XSSFWorkbook();
+
+                ISheet sheet1 = workbook.CreateSheet("Sheet1");
+
+                sheet1.AddMergedRegion(new CellRangeAddress(0, 0, 0, 10));
+                var rowIndex = 0;
+                IRow row = sheet1.CreateRow(rowIndex);
+                row.Height = 30 * 80;
+                row.CreateCell(0).SetCellValue("this is content");
+                sheet1.AutoSizeColumn(0);
+                rowIndex++;
+
+                var sheet2 = workbook.CreateSheet("Sheet2");
+                var style1 = workbook.CreateCellStyle();
+                style1.FillForegroundColor = HSSFColor.Blue.Index2;
+                style1.FillPattern = FillPattern.SolidForeground;
+
+                var style2 = workbook.CreateCellStyle();
+                style2.FillForegroundColor = HSSFColor.Yellow.Index2;
+                style2.FillPattern = FillPattern.SolidForeground;
+
+                var cell2 = sheet2.CreateRow(0).CreateCell(0);
+                cell2.CellStyle = style1;
+                cell2.SetCellValue(0);
+
+                cell2 = sheet2.CreateRow(1).CreateCell(0);
+                cell2.CellStyle = style2;
+                cell2.SetCellValue(1);
+
+                workbook.Write(fs);
+            }
+
+
+            return RedirectToAction("Index", "Appointments");
+        }
+
+        public IActionResult AppointmentWord(int doctorPage = 1, string searchName = null, string searchEmail = null, string searchPhone = null, string searchDate = null)
+        {
+
+            System.Security.Claims.ClaimsPrincipal currentUser = this.User;
+            var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            AppointmentViewModel appointmentVM = new AppointmentViewModel()
+            {
+                Appointments = new List<Models.Appointments>()
+            };
+
+            appointmentVM.Appointments = _db.Appointments.Include(a => a.DoctorUser).ToList();
+            if (User.IsInRole(SD.AdminEndUser))
+            {
+                appointmentVM.Appointments = appointmentVM.Appointments.Where(a => a.DoctorId == claim.Value).ToList();
+            }
+
+            if (searchName != null)
+            {
+                appointmentVM.Appointments = appointmentVM.Appointments.Where(a => a.PatientName.ToLower().Contains(searchName.ToLower())).ToList();
+            }
+            if (searchEmail != null)
+            {
+                appointmentVM.Appointments = appointmentVM.Appointments.Where(a => a.PatientEmail.ToLower().Contains(searchEmail.ToLower())).ToList();
+            }
+            if (searchPhone != null)
+            {
+                appointmentVM.Appointments = appointmentVM.Appointments.Where(a => a.PatientPhoneNumber.ToLower().Contains(searchPhone.ToLower())).ToList();
+            }
+            if (searchDate != null)
+            {
+                try
+                {
+                    DateTime appDate = Convert.ToDateTime(searchDate);
+                    appointmentVM.Appointments = appointmentVM.Appointments.Where(a => a.AppointmentDate.ToShortDateString().Equals(appDate.ToShortDateString())).ToList();
+                }
+                catch (Exception ex)
+                {
+
+                }
+
+            }
+
+            var count = appointmentVM.Appointments.Count;
+
+            appointmentVM.Appointments = appointmentVM.Appointments.OrderBy(p => p.AppointmentDate)
+                .Skip((doctorPage - 1) * PageSize)
+                .Take(PageSize).ToList();
+
+
+
+
+            var newFile = homePath + "\\Downloads\\" + "AppointmentWord.docx";
+            
+            using (var fs = new FileStream(newFile, FileMode.Create, FileAccess.Write))
+            {
+                XWPFDocument doc = new XWPFDocument();
+                var p0 = doc.CreateParagraph();
+                p0.Alignment = ParagraphAlignment.CENTER;
+                XWPFRun r0 = p0.CreateRun();
+                r0.FontFamily = "Arial";
+                r0.FontSize = 18;
+                r0.IsBold = true;
+                r0.SetText("Термини датум: " + DateTime.Now.ToString("MM/dd/yyyy HH:mm"));
+
+                var p1 = doc.CreateParagraph();
+                p1.Alignment = ParagraphAlignment.LEFT;
+                p1.IndentationFirstLine = 500;
+                XWPFRun r1 = p1.CreateRun();
+                r1.FontFamily = "Arial";
+                r1.FontSize = 12;
+                r1.IsBold = true;
+
+                StringBuilder stringBuilder = new StringBuilder();
+
+
+                foreach (Appointments appointment in appointmentVM.Appointments) {
+                    stringBuilder.Append(appointment.DoctorUser.Name);
+                    stringBuilder.AppendLine();
+                }
+                r1.SetText(stringBuilder.ToString());
+                r1.AppendText("^l");
+                r1.AppendText("Dimtiar Mielski");
+                doc.Write(fs);
+            }
+
+            return RedirectToAction("Index", "Appointments");
+        }
     }
 }
